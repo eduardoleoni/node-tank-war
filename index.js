049612fe5e0b10b8e7bada1a97d5d7299cb1027c
eduardoleoni <eduardoleoni@192.168.1.7>
2015-05-07 09:58:24 +0100

    procfile port

--- index.js
+++ index.js
@@ -57,8 +57,8 @@ io.on('connection', function(socket){
 io.emit('some event', { for: 'everyone' });
 
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(5000, function(){
+  console.log('listening on *:5000');
 });
 
 
